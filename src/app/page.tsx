3bb641b4af7c5876ef44f1f07601e5a4b109e8ce
TechnoVirtuoso <zeeshan.coworker@gmail.com>
2024-11-19 11:00:59 -0800

    update bg grid mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,32 @@ import WhyUs from "./components/why-us";
 import ActionData from "./components/action-data";
 import CtaBlock from "./components/cta-block";
 import Footer from "./footer";
+import bgMobileGrid from '@/assets/images/mobile-bg-grid.png';
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 700);
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+
   return (
   
     <div className="main-wrapper">
       <Header/>
       <HeroBanner/>
-      <div className="wrapper-block" style={{ backgroundImage: `url(${bg_grid.src})`}}>
+      <div className="wrapper-block" style={{ backgroundImage: `url(${isMobile ? bgMobileGrid.src : bg_grid.src})`}}>
         <Icons/>
         <ImageBlurb/>
         <PricingBlock/>
